60c6f4486ea8d3297b24cb3e910747ec0d65b12c
Junior-267 <jorgenapolijunior@gmail.com>
2026-08-31 15:17:01 -0300

    Update register.php

--- Encriptacion/public/register.php
+++ Encriptacion/public/register.php
@@ -7,14 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = trim($_POST['username']);
   $email = trim($_POST['email']);
   $password = $_POST['password'];
-  $hashed_password = password_hash($password , PASSWORD_DEFAULT);
-  $stmt = $pdo->prepare("INSERT INTO users (username , email, password) VALUES (?,
-  ?, ?)");
-  if ($stmt->execute([$username , $email, $hashed_password])) {
-    $success = '¡Registro exitoso!';
-    } else {
-      $error = 'Registro fallido. Por favor, intentelo nuevamente.';
-  }
   $confirm_password = $_POST['confirm_password'];
   if (empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
     $error = 'Todos los campos son requeridos';
