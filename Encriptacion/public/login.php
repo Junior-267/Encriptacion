c98e8a5dcfa891420cc682b2560719b752676e60
Junior-267 <jorgenapolijunior@gmail.com>
2026-08-31 16:56:15 -0300

    Update login.php

--- Encriptacion/public/login.php
+++ Encriptacion/public/login.php
@@ -1,10 +1,18 @@
 <?php
 //Iniciar sesion - Guardar datos del usuario en $_SESSION
 if ($user && password_verify($password , $user['password'])) {
-$_SESSION['user_id'] = $user['id'];
-$_SESSION['username'] = $user['username'];
-$_SESSION['login_time'] = date('Y-m-d H:i:s');
-header('Location: dashboard.php');
-exit;
+  require_once '../config/crypto.php';
+   //Generacion de un token pseudoaleatorio seguro
+  $token_original = "SESION_" . bin2hex(random_bytes(16));
+  // Cifrado asimétrico mediante clave pública
+  $token_cifrado = cifrarRSA($token_original);
+  $_SESSION['user_id'] = $user['id'];
+  $_SESSION['username'] = $user['username'];
+  $_SESSION['login_time'] = date('Y-m-d H:i:s');
+  $_SESSION['token_rsa_cifrado'] = $token_cifrado;
+  $_SESSION['token_rsa_descifrado'] = $token_descifrado;
+ 
+  header('Location: dashboard.php');
+  exit;
 }
 ?>
